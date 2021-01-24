8b3be5b55e6d8699a2dcc4f13404b389791c4802
Arsh Sahzad <admin@arsh.dev>
2021-01-25 00:58:11 +0530

    minor update

--- src/pages/globalStyle.jsx
+++ src/pages/globalStyle.jsx
@@ -1,6 +1,4 @@
 import {createGlobalStyle} from 'styled-components'
-
-
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
     ::root {
